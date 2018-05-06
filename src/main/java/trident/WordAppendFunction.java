7aff23e73c7d1e6288c94132b339da358c4f9a3b
Yogesh Walia <yogeshwalia@Yogeshs-MacBook-Air.local>
2018-05-06 10:19:31 +0530

    Adding Trident Topology Basic Function

---
+++ src/main/java/trident/WordAppendFunction.java
@@ -0,0 +1,14 @@
+package trident;
+
+import org.apache.storm.trident.operation.BaseFunction;
+import org.apache.storm.trident.operation.TridentCollector;
+import org.apache.storm.trident.tuple.TridentTuple;
+import org.apache.storm.tuple.Values;
+
+public class WordAppendFunction extends BaseFunction {
+
+    @Override
+    public void execute(TridentTuple tridentTuple, TridentCollector tridentCollector) {
+        tridentCollector.emit(new Values(tridentTuple.get(0) + " Processed..."));
+    }
+}
